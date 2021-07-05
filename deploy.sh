c844d7fa4bbd6c3f0055296869e1f54eb88667b2
andylim0221 <limchinhuat0221@gmail.com>
2021-07-05 23:38:05 +0800

    edit pipeline and docs

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "Check docker"
+echo "Run container"
 
-if [[ $(which docker) && $(docker --version) ]]; then
-    echo "Docker installed. Build docker image from this project."
-    docker build -t example .
-    docker run -d example -p 8000:8000
-    docker ps
-  else
-    echo "Install docker"
-fi
+docker build -t example .
+docker run -d example -p 8000:8000
+docker ps
